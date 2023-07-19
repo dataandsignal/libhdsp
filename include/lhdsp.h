20b8290d818f38fbbe06d9f5ce2ec703962d72c4
Piotr Gregor <piotr@dataandsignal.com>
2023-07-19 22:40:55 +0100

    Add src include

---
+++ include/lhdsp.h
@@ -0,0 +1,69 @@
+/*
+ * This file is part of libhdsp - Handy DSP routines library.
+ *
+ * Copyright (c) 2023 Data And Signal - IT Solutions
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ *   Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Piotr Gregor <piotr@dataandsignal.com>
+ * Data And Signal - IT Solutions
+ *
+ */
+
+
+#ifndef LHDSP_LHDSP_H
+#define LHDSP_LHDSP_H
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
+
+#include <math.h>
+#include <float.h>
+
+#define LHDSP_VER_STRING PACKAGE_STRING
+#define LHDSP_VERSION PACKAGE_VERSION
+
+
+/**
+ * Says hi.
+ */
+void lhdsp_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* LHDSP_LHDSP_H */
