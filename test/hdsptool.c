e9eff06b0d3c90d233839fb89a48e72ba7ae105c
Piotr Gregor <piotr@dataandsignal.com>
2023-08-28 13:58:17 +0100

    Denoise without filtering, write results

--- test/hdsptool.c
+++ test/hdsptool.c
@@ -58,7 +58,6 @@
 #define SAMPLES_PER_10MS_FRAME_OF_48000HZ 480
 #define BUFLEN 2000
 
-DenoiseState *rnnoise;
 
 static void usage(const char *name) {
     if (name == NULL)
@@ -84,8 +83,11 @@ int main(int argc, char **argv) {
     int filter_len = 0;
     int samples_per_ptime_of_48khz_frame = 0;
     FILE *f_in = NULL;
-    FILE *f_out_x = NULL, *f_out_x_upsampled = NULL,
-        *f_out_x_upsampled_filtered = NULL, *f_out_x_upsampled_filtered_denoised = NULL, *f_out_x_upsampled_filtered_downsampled = NULL;
+    FILE *f_out_x = NULL, *f_out_x_upsampled = NULL, *f_out_x_upsampled_denoised = NULL,
+        *f_out_x_upsampled_denoised_downsampled = NULL,
+        *f_out_x_upsampled_filtered = NULL, *f_out_x_upsampled_filtered_denoised = NULL,
+        *f_out_x_upsampled_filtered_denoised_downsampled = NULL,
+        *f_out_x_upsampled_filtered_downsampled = NULL;
     size_t n = 0, n_total = 0;
     int16_t frame_in[TARGET_SAMPLE_RATE] = {0};
     double frame_out[TARGET_SAMPLE_RATE] = {0};
@@ -97,9 +99,13 @@ int main(int argc, char **argv) {
     int upsample_factor = 0;
     char fname_x[BUFLEN] = {0};
     char fname_x_u[BUFLEN] = {0};
+    char fname_x_u_den[BUFLEN] = {0};
+    char fname_x_u_den_dwns[BUFLEN] = {0};
     char fname_x_u_f[BUFLEN] = {0};
     char fname_x_u_f_den[BUFLEN] = {0};
+    char fname_x_u_f_den_dwns[BUFLEN] = {0};
     char fname_x_u_f_dwns[BUFLEN] = {0};
+    DenoiseState *rnnoise1 = NULL, *rnnoise2 = NULL;
     int denoising = 0;
 
     if (argc < 5) {
@@ -139,8 +145,11 @@ int main(int argc, char **argv) {
 
     snprintf(fname_x, BUFLEN - 1, "%dms_%d_x.raw", ptime_ms, filter_len);
     snprintf(fname_x_u, BUFLEN - 1, "%dms_%d_x_u.raw", ptime_ms, filter_len);
+    snprintf(fname_x_u_den, BUFLEN - 1, "%dms_%d_x_u_den.raw", ptime_ms, filter_len);
+    snprintf(fname_x_u_den_dwns, BUFLEN - 1, "%dms_%d_x_u_den_dwns.raw", ptime_ms, filter_len);
     snprintf(fname_x_u_f, BUFLEN - 1, "%dms_%d_x_u_f.raw", ptime_ms, filter_len);
     snprintf(fname_x_u_f_den, BUFLEN - 1, "%dms_%d_x_u_f_den.raw", ptime_ms, filter_len);
+    snprintf(fname_x_u_f_den_dwns, BUFLEN - 1, "%dms_%d_x_u_f_den_dwns.raw", ptime_ms, filter_len);
     snprintf(fname_x_u_f_dwns, BUFLEN - 1, "%dms_%d_x_u_f_dwns.raw", ptime_ms, filter_len);
 
     f_in = fopen(f_in_name, "rb");
@@ -148,7 +157,10 @@ int main(int argc, char **argv) {
     f_out_x_upsampled = fopen(fname_x_u, "wb");
     f_out_x_upsampled_filtered = fopen(fname_x_u_f, "wb");
     if (denoising) {
+        f_out_x_upsampled_denoised = fopen(fname_x_u_den, "wb");
+        f_out_x_upsampled_denoised_downsampled = fopen(fname_x_u_den_dwns, "wb");
         f_out_x_upsampled_filtered_denoised = fopen(fname_x_u_f_den, "wb");
+        f_out_x_upsampled_filtered_denoised_downsampled = fopen(fname_x_u_f_den_dwns, "wb");
     }
     f_out_x_upsampled_filtered_downsampled = fopen(fname_x_u_f_dwns, "wb");
 
@@ -182,8 +194,9 @@ int main(int argc, char **argv) {
     }
 
     if (denoising) {
-        rnnoise = rnnoise_create(NULL);
-        if (!rnnoise) {
+        rnnoise1 = rnnoise_create(NULL);
+        rnnoise2 = rnnoise_create(NULL);
+        if (!rnnoise1 || !rnnoise2) {
             fprintf(stderr, "Failed to create RNNoise\n");
             goto fail;
         }
@@ -191,7 +204,7 @@ int main(int argc, char **argv) {
 
     while (samples_in == (n = fread(frame_in, sizeof(int16_t), samples_in, f_in))) {
         int m = 0;
-        int16_t buffer[TARGET_SAMPLE_RATE] = {0};
+        int16_t buffer[TARGET_SAMPLE_RATE] = {0}, buffer1[TARGET_SAMPLE_RATE] = {0}, buffer2[TARGET_SAMPLE_RATE] = {0};
         memset(rnnoise_in, 0, sizeof(rnnoise_in));
         memset(rnnoise_out, 0, sizeof(rnnoise_out));
         memset(frame_out, 0, sizeof(frame_out));
@@ -207,18 +220,50 @@ int main(int argc, char **argv) {
         }
 
         // process frame_in
-        if (HDSP_STATUS_OK != hdsp_upsample_int16(frame_in, samples_in, upsample_factor, buffer, samples_per_ptime_of_48khz_frame)) {
+        if (HDSP_STATUS_OK != hdsp_upsample_int16(frame_in, samples_in, upsample_factor, buffer,
+                                                  samples_per_ptime_of_48khz_frame)) {
             fprintf(stderr, "Failed to upsample\n");
             goto fail;
         }
 
         // write upsampled
-        if (samples_per_ptime_of_48khz_frame < fwrite(buffer, sizeof(int16_t), samples_per_ptime_of_48khz_frame, f_out_x_upsampled)) {
+        if (samples_per_ptime_of_48khz_frame < fwrite(buffer, sizeof(int16_t), samples_per_ptime_of_48khz_frame,
+                                                      f_out_x_upsampled)) {
             fprintf(stderr, "Failed to write\n");
             goto fail;
         }
 
-        if (HDSP_STATUS_OK != hdsp_fir_filter(buffer, samples_per_ptime_of_48khz_frame, &filter, frame_out, samples_per_ptime_of_48khz_frame)) {
+        if (denoising) {
+            memset(rnnoise_in, 0, sizeof(rnnoise_in));
+            memset(rnnoise_out, 0, sizeof(rnnoise_out));
+            memset(buffer1, 0, sizeof(buffer1));
+            memset(buffer2, 0, sizeof(buffer2));
+            hdsp_int16_2_float(buffer, samples_per_ptime_of_48khz_frame, rnnoise_in);
+            rnnoise_process_frame(rnnoise1, rnnoise_out, rnnoise_in);
+            hdsp_float_2_int16(rnnoise_out, samples_per_ptime_of_48khz_frame, buffer1);
+
+            // write upsampled, denoised
+            if (samples_per_ptime_of_48khz_frame < fwrite(buffer1, sizeof(int16_t), samples_per_ptime_of_48khz_frame,
+                                                          f_out_x_upsampled_denoised)) {
+                fprintf(stderr, "Failed to write\n");
+                goto fail;
+            }
+
+            if (HDSP_STATUS_OK != hdsp_downsample_int16(buffer1, samples_per_ptime_of_48khz_frame,
+                                                         upsample_factor, buffer2, samples_in)) {
+                fprintf(stderr, "Failed to downsample\n");
+                goto fail;
+            }
+
+            // write upsampled, denoised, downsampled
+            if (samples_in < fwrite(buffer2, sizeof(int16_t), samples_in, f_out_x_upsampled_denoised_downsampled)) {
+                fprintf(stderr, "Failed to write\n");
+                goto fail;
+            }
+        }
+
+        if (HDSP_STATUS_OK != hdsp_fir_filter(buffer, samples_per_ptime_of_48khz_frame, &filter, frame_out,
+                                              samples_per_ptime_of_48khz_frame)) {
             fprintf(stderr, "Failed to filter\n");
             goto fail;
         }
@@ -230,24 +275,43 @@ int main(int argc, char **argv) {
         }
 
         // write upsampled, filtered
-        if (samples_per_ptime_of_48khz_frame < fwrite(buffer, sizeof(int16_t), samples_per_ptime_of_48khz_frame, f_out_x_upsampled_filtered)) {
+        if (samples_per_ptime_of_48khz_frame < fwrite(buffer, sizeof(int16_t), samples_per_ptime_of_48khz_frame,
+                                                      f_out_x_upsampled_filtered)) {
             fprintf(stderr, "Failed to write\n");
             goto fail;
         }
 
         if (denoising) {
+            memset(rnnoise_in, 0, sizeof(rnnoise_in));
+            memset(rnnoise_out, 0, sizeof(rnnoise_out));
+            memset(buffer1, 0, sizeof(buffer1));
+            memset(buffer2, 0, sizeof(buffer2));
             hdsp_double_2_float(frame_out, samples_per_ptime_of_48khz_frame, rnnoise_in);
-            rnnoise_process_frame(rnnoise, rnnoise_out, rnnoise_in);
-            hdsp_float_2_int16(rnnoise_out, samples_per_ptime_of_48khz_frame, buffer);
+            rnnoise_process_frame(rnnoise2, rnnoise_out, rnnoise_in);
+            hdsp_float_2_int16(rnnoise_out, samples_per_ptime_of_48khz_frame, buffer1);
 
             // write upsampled, filtered, denoised
-            if (samples_per_ptime_of_48khz_frame < fwrite(buffer, sizeof(int16_t), samples_per_ptime_of_48khz_frame, f_out_x_upsampled_filtered_denoised)) {
+            if (samples_per_ptime_of_48khz_frame < fwrite(buffer1, sizeof(int16_t), samples_per_ptime_of_48khz_frame,
+                                                          f_out_x_upsampled_filtered_denoised)) {
+                fprintf(stderr, "Failed to write\n");
+                goto fail;
+            }
+
+            if (HDSP_STATUS_OK != hdsp_downsample_int16(buffer1, samples_per_ptime_of_48khz_frame,
+                                                        upsample_factor, buffer2, samples_in)) {
+                fprintf(stderr, "Failed to downsample\n");
+                goto fail;
+            }
+
+            // write upsampled, denoised, downsampled
+            if (samples_in < fwrite(buffer2, sizeof(int16_t), samples_in, f_out_x_upsampled_filtered_denoised_downsampled)) {
                 fprintf(stderr, "Failed to write\n");
                 goto fail;
             }
         }
 
-        if (HDSP_STATUS_OK != hdsp_downsample_double(frame_out, samples_per_ptime_of_48khz_frame, upsample_factor, frame_out_downsampled, samples_in)) {
+        if (HDSP_STATUS_OK != hdsp_downsample_double(frame_out, samples_per_ptime_of_48khz_frame, upsample_factor,
+                                                     frame_out_downsampled, samples_in)) {
             fprintf(stderr, "Failed to downsample\n");
             goto fail;
         }
@@ -276,17 +340,29 @@ int main(int argc, char **argv) {
     if (f_out_x_upsampled) {
         fclose(f_out_x_upsampled);
     }
+    if (f_out_x_upsampled_denoised) {
+        fclose(f_out_x_upsampled_denoised);
+    }
+    if (f_out_x_upsampled_denoised_downsampled) {
+        fclose(f_out_x_upsampled_denoised_downsampled);
+    }
     if (f_out_x_upsampled_filtered) {
         fclose(f_out_x_upsampled_filtered);
     }
     if (f_out_x_upsampled_filtered_denoised) {
         fclose(f_out_x_upsampled_filtered_denoised);
     }
+    if (f_out_x_upsampled_filtered_denoised_downsampled) {
+        fclose(f_out_x_upsampled_filtered_denoised_downsampled);
+    }
     if (f_out_x_upsampled_filtered_downsampled) {
         fclose(f_out_x_upsampled_filtered_downsampled);
     }
-    if (rnnoise) {
-        rnnoise_destroy(rnnoise);
+    if (rnnoise1) {
+        rnnoise_destroy(rnnoise1);
+    }
+    if (rnnoise2) {
+        rnnoise_destroy(rnnoise2);
     }
     printf("Done. (frames: %d, bytes total: %zu)\n", k, n_total * sizeof(int16_t));
     return 0;
@@ -301,17 +377,29 @@ fail:
     if (f_out_x_upsampled) {
         fclose(f_out_x_upsampled);
     }
+    if (f_out_x_upsampled_denoised) {
+        fclose(f_out_x_upsampled_denoised);
+    }
+    if (f_out_x_upsampled_denoised_downsampled) {
+        fclose(f_out_x_upsampled_denoised_downsampled);
+    }
     if (f_out_x_upsampled_filtered) {
         fclose(f_out_x_upsampled_filtered);
     }
     if (f_out_x_upsampled_filtered_denoised) {
         fclose(f_out_x_upsampled_filtered_denoised);
     }
+    if (f_out_x_upsampled_filtered_denoised_downsampled) {
+        fclose(f_out_x_upsampled_filtered_denoised_downsampled);
+    }
     if (f_out_x_upsampled_filtered_downsampled) {
         fclose(f_out_x_upsampled_filtered_downsampled);
     }
-    if (rnnoise) {
-        rnnoise_destroy(rnnoise);
+    if (rnnoise1) {
+        rnnoise_destroy(rnnoise1);
+    }
+    if (rnnoise2) {
+        rnnoise_destroy(rnnoise2);
     }
     exit(EXIT_FAILURE);
 }
