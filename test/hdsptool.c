c96f17e49f12d9a485b068f8676a1b4d9ff491a2
Piotr Gregor <piotr@dataandsignal.com>
2023-09-20 10:37:48 +0100

    Comments

--- test/hdsptool.c
+++ test/hdsptool.c
@@ -47,6 +47,9 @@
  *          frequency aliasing effect that is a result of upsampling
  *      PTms_x_u_f_d.raw - upsampled, filtered and downsampled to original sampling rate (8 or 16 kHz)
  * PTms_ will resolve to <ptime ms>ms_ where ptime is passed as a command parameter.
+ *
+ * Command 'upsamplef' is similar, but accepts any sampling rate and let's to define filter length.
+ * Commands 'denoise' and 'denoisef' work on the same principle, but additionally perform denoising with RNNoise.
  */
 
 
