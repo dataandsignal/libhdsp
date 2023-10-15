14bbabfec8b3734be06e3d05a99e3a1ea9c1f84e
Piotr Gregor <piotr@dataandsignal.com>
2023-10-15 16:21:25 +0100

    Fix typo

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Run this file to set up the build system: configure, makefiles, etc.
 set -e
-echo "Updating build configuration files for libhddsp, please wait...."
+echo "Updating build configuration files for libhdsp, please wait...."
 
 autoreconf -isf
